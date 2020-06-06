d3544015ddcdea1ff3ac6c1f0ebfd2f7e895dc9a
Aastha Gupta <aastha.gupta@railyatri.in>
2020-06-06 10:33:48 +0530

    Placeholder for imageview

--- app/src/main/java/com/aastha/zomatosearch/utils/DataBindingAdapters.kt
+++ app/src/main/java/com/aastha/zomatosearch/utils/DataBindingAdapters.kt
@@ -5,7 +5,10 @@ import android.text.TextUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.aastha.zomatosearch.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 
 object DataBindingAdapters {
 
@@ -13,7 +16,15 @@ object DataBindingAdapters {
     @BindingAdapter("loadURL")
     fun loadURL(imageView: ImageView, url: String?) {
         if (!TextUtils.isEmpty(url)) {
-            Glide.with(imageView.context).load(url).into(imageView)
+            Glide.with(imageView.context).load(url)
+                .apply(
+                    RequestOptions().placeholder(R.drawable.placeholder)
+                        .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                        .error(R.drawable.placeholder)
+                )
+                .into(imageView)
+        } else {
+            imageView.setImageResource(R.drawable.placeholder)
         }
     }
 
